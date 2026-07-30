9136356efb334ac6a9e7b92ed33a602fffcda418
leenb666666 <181578840+leenb666666@users.noreply.github.com>
2026-07-30 12:04:43 +0800

    Publish SentenceFlow web app

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = "sentenceflow-v1";
+self.addEventListener("install", event => {
+  const scope = self.registration.scope;
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll([scope, `${scope}manifest.webmanifest`, `${scope}favicon.svg`])));
+});
+self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(response => {
+    const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)); return response;
+  }).catch(() => caches.match(self.registration.scope))));
+});
